1a07dc57af1ddfd0362bfb70e4ca40ee8e701c55
jasonandy888 <260571718+jasonandy888@users.noreply.github.com>
2026-05-22 13:40:10 +0800

    Add files via upload

--- trunk/configs/boards/K2P/board.h
+++ trunk/configs/boards/K2P/board.h
@@ -1,9 +1,9 @@
 /* K2P */
 
-#define BOARD_PID		"K2P"
-#define BOARD_NAME		"K2P"
-#define BOARD_DESC		"PHICOMM K2P Wireless Router"
-#define BOARD_VENDOR_NAME	"PHICOMM"
+#define BOARD_PID		"FreeOK"
+#define BOARD_NAME		"FreeOK"
+#define BOARD_DESC		"FreeOK"
+#define BOARD_VENDOR_NAME	"FreeOK"
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
 #define BOARD_BOOT_TIME		30
